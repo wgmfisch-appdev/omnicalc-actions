58f48a69d354382277c4e29bb5ced2724fc059cd
Raghu Betina <raghu@firstdraft.com>
2018-10-11 11:46:32 -0500

    Update meteorologist_controller.rb

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -3,13 +3,13 @@ require 'open-uri'
 class MeteorologistController < ApplicationController
   def street_to_weather
     @street_address = params.fetch("user_street_address")
-    url_safe_street_address = URI.encode(@street_address)
+    sanitized_street_address = URI.encode(@street_address)
 
     # ==========================================================================
     # Your code goes below.
     # The street address the user input is in the string @street_address.
-    # A URL-safe version of the street address, with spaces and other illegal
-    #   characters removed, is in the string url_safe_street_address.
+    # A sanitized version of the street address, with spaces and other illegal
+    #   characters removed, is in the string sanitized_street_address.
     # ==========================================================================
 
     @current_temperature = "Replace this string with your answer"

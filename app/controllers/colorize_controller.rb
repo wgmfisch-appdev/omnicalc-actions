04b2e735254121b558d0cf28a1ea0ceb3798d9e7
Jelani Woods <jmw686@nyu.edu>
2018-10-25 22:37:44 -0500

    strip white space from image url

--- app/controllers/colorize_controller.rb
+++ app/controllers/colorize_controller.rb
@@ -1,14 +1,14 @@
 class ColorizeController < ApplicationController
   def colorize
     input = {
-      image: params.fetch("image_url")
+      image: params.fetch("image_url").strip
     }
 
     # ================================================================================
     # Your code goes below.
     # ================================================================================
 
-    @original_image_url = params.fetch("image_url")
+    @original_image_url = params.fetch("image_url").strip
     @colorized_image_url = "Replace this string with your answer"
 
     # ================================================================================

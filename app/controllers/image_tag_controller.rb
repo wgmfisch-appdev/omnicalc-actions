42b584f4ac06499d6962e1966f0299f2da424f32
Jelani Woods <jmw686@nyu.edu>
2018-10-25 22:38:27 -0500

    strip white space from image url

--- app/controllers/image_tag_controller.rb
+++ app/controllers/image_tag_controller.rb
@@ -1,14 +1,14 @@
 class ImageTagController < ApplicationController
   def image_tag
     input = {
-      image: params.fetch("image_url")
+      image: params.fetch("image_url").strip
     }
 
     # ================================================================================
     # Your code goes below.
     # ================================================================================
 
-    @original_image_url = params.fetch("image_url")
+    @original_image_url = params.fetch("image_url").strip
     @tag_hashes = "Replace with the array containing the hashes representing tags"
 
     # ================================================================================
